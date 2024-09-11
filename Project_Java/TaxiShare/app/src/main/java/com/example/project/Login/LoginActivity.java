3d685f9476dcd3a2bdf1d85bf5463bc27fad631a
Tae young Kim <tyk0707@gmail.com>
2024-09-11 16:28:55 +0900

    Ver1.1

--- Project_Java/TaxiShare/app/src/main/java/com/example/project/Login/LoginActivity.java
+++ Project_Java/TaxiShare/app/src/main/java/com/example/project/Login/LoginActivity.java
@@ -1,17 +1,22 @@
 package com.example.project.Login;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import com.example.project.MainActivity;
 import com.example.project.R;
@@ -31,9 +36,12 @@ public class LoginActivity extends AppCompatActivity {
     private FirebaseUser firebaseUser;
     private FirebaseAuth myAuth;
 
+    ProgressDialog progressDialog;
+
     // 버튼
-    Button btn_login;
+    AppCompatButton btn_login;
     TextView text_find, text_signUp;
+    CheckBox checkBox;
 
     EditText ID,PW;
 
@@ -54,6 +62,9 @@ public class LoginActivity extends AppCompatActivity {
 
         ID = findViewById(R.id.ID);
         PW = findViewById(R.id.PW);
+        checkBox = findViewById(R.id.checkBox);
+
+        progressDialog = new ProgressDialog(this);
 
         // 로그인 버튼
         btn_login = findViewById(R.id.btn_login);
@@ -64,22 +75,41 @@ public class LoginActivity extends AppCompatActivity {
                 String id = ID.getText().toString();
                 String pw = PW.getText().toString();
 
-                if (TextUtils.isEmpty(id))
-                    showToast("아이디를 입력하세요!");
-                else if (TextUtils.isEmpty(pw))
-                    showToast("비밀번호를 입력하세요!");
+                if (TextUtils.isEmpty(id)) {
+                    ID.setError("아이디를 입력하세요!");
+                    ID.requestFocus();
+                }
+                else if (TextUtils.isEmpty(pw)) {
+                    PW.setError("비밀번호를 입력하세요!");
+                    PW.requestFocus();
+                }
                 else{
-                    myAuth.signInWithEmailAndPassword(id, pw).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    String email = id + "@cku.ac.kr";
+                    progressDialog.setMessage("로그인 중");
+                    progressDialog.show();
+                    myAuth.signInWithEmailAndPassword(email, pw).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if(task.isSuccessful()){
+                                if(checkBox.isChecked()){
+                                    SharedPreferences sharedPreferences = getSharedPreferences("loginInfo", MODE_PRIVATE);
+                                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                                    editor.putBoolean("autoLogin", true);
+                                    editor.putString("id", email);
+                                    editor.putString("pw", pw);
+                                    editor.apply();
+                                }
                                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                                 startActivity(intent);
                                 showToast("로그인 성공");
+                                progressDialog.dismiss();
+                                finish();
                             }else{
                                 showToast("로그인 정보가 일치하지 않습니다.");
+                                progressDialog.dismiss();
                                 ID.setText("");
                                 PW.setText("");
+                                ID.requestFocus();
                             }
                         }
                     });
@@ -93,6 +123,16 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(LoginActivity.this, FindActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // 회원가입
+        text_signUp = findViewById(R.id.text_signUp);
+        text_signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
                 startActivity(intent);
             }
         });
--- Project_Java/TaxiShare/app/src/main/java/com/example/project/Login/SignUpActivity.java
+++ Project_Java/TaxiShare/app/src/main/java/com/example/project/Login/SignUpActivity.java
@@ -1,10 +1,14 @@
 package com.example.project.Login;
 
+import android.app.ProgressDialog;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import com.example.project.R;
 import com.google.firebase.Firebase;
@@ -27,8 +31,13 @@ public class SignUpActivity extends AppCompatActivity {
     private DatabaseReference databaseRef;
     private Uri imgUri;
 
+    private ProgressDialog progressDialog;
+
     // 이메일 검증
     private boolean verifyEmail = false;
+
+    EditText ID, PW, re_PW, nickName;
+    AppCompatButton btn_signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +48,37 @@ public class SignUpActivity extends AppCompatActivity {
         user = FirebaseAuth.getInstance().getCurrentUser();
         storageRef = FirebaseStorage.getInstance().getReference();
         databaseRef = FirebaseDatabase.getInstance().getReference();
+
+        progressDialog = new ProgressDialog(this);
+
+        ID = findViewById(R.id.ID);
+        String id = ID.getText().toString();
+        PW = findViewById(R.id.PW);
+        String pw = PW.getText().toString();
+        re_PW = findViewById(R.id.re_PW);
+        String re_pw = re_PW.getText().toString();
+        nickName = findViewById(R.id.nickName);
+        String nick = nickName.getText().toString();
+
+        btn_signUp = findViewById(R.id.btn_signUp);
+        if(TextUtils.isEmpty(id)) {
+            ID.setError("아이디를 입력하세요");
+            ID.requestFocus();
+        }
+        else if(TextUtils.isEmpty(pw)) {
+            PW.setError("비밀번호를 입력하세요");
+            PW.requestFocus();
+        }else if (pw.length() < 8){
+            PW.setError("비밀번호를 8글자 이상 입력하세요");
+        } else if(TextUtils.isEmpty(re_pw)) {
+            re_PW.setError("비밀번호를 다시 입력하세요");
+            re_PW.requestFocus();
+        }else if (TextUtils.isEmpty(nick)){
+            nickName.setError("닉네임을 입력하세요");
+            nickName.requestFocus();
+        }else if (pw != re_pw) {
+            re_PW.setError("비밀번호를 동일하게 입력하세요");
+        }
 
     }
 
